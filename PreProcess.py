8de48a4223bcd9f69c7888c7f2e74002a7abc6d0
Leandro <leandroglez39@gmail.com>
2019-11-04 17:18:11 -0500

    Organizando PreProcesamiento

--- PreProcess.py
+++ PreProcess.py
@@ -3,26 +3,32 @@ import pdfminer
 import os
 from nltk.corpus import stopwords
 
-#open file
-f = open('document.txt')
-raw = f.read()
-
-#tokening
-tokens = nltk.word_tokenize(raw)
-
-#cargar stopwords
-stop_words = set(stopwords.words('english'))
-
-#Stemming
-porter = nltk.PorterStemmer()
-tokens_stem = [porter.stem(t) for t in tokens ]
-
-#Lemmatization
-wnl = nltk.WordNetLemmatizer()
-tokens_lem = [wnl.lemmatize(t) for t in tokens_stem]
 
 
+def PreProces(text):
+    #tokening
+    tokens = nltk.word_tokenize(text)
 
-print(tokens)
+    #Remove stopwords
+    tokens_stopw = [x for x in tokens if x.lower() not in stop_words]
 
-print(stop_words)
+    #Stemming
+    porter = nltk.PorterStemmer()
+    tokens_stem = [porter.stem(t) for t in tokens_stopw ]
+
+    #Lemmatization
+    wnl = nltk.WordNetLemmatizer()
+    tokens_lem = [wnl.lemmatize(t) for t in tokens_stem]
+
+    return tokens_lem
+
+
+if __name__ == '__main__':
+    # open file
+    f = open('document.txt')
+    raw = f.read()
+
+    # cargar stopwords
+    stop_words = set(stopwords.words('english'))
+
+    print(PreProces(raw))
