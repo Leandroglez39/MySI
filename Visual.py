b18208ed981bcb80ae70b8495c2aed422f2fd44f
Leandro <leandroglez39@gmail.com>
2019-11-04 18:25:05 -0500

    Añadido segundo boton

--- Visual.py
+++ Visual.py
@@ -1,5 +1,11 @@
+import pickle
+
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QGridLayout, QLabel, QLineEdit, QMessageBox, QPushButton, QTextEdit, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import (QDialog, QFileDialog, QGridLayout, QHBoxLayout,
+        QLabel, QLineEdit, QMessageBox, QPushButton, QTextEdit, QVBoxLayout,
+        QWidget)
+
+from SI import index
 
 
 class AddressBook(QWidget):
@@ -20,9 +26,14 @@ class AddressBook(QWidget):
         self.addButton = QPushButton("&Run")
         self.addButton.show()
 
+        self.loadButton = QPushButton("&Load...")
+        self.loadButton.setToolTip("Load dir from a file")
+
         self.addButton.clicked.connect(self.addContact)
+        self.loadButton.clicked.connect(self.loadFromFile)
 
         buttonLayout1 = QVBoxLayout()
+        buttonLayout1.addWidget(self.loadButton)
         buttonLayout1.addWidget(self.addButton, Qt.AlignTop)
         buttonLayout1.addStretch()
 
@@ -33,7 +44,7 @@ class AddressBook(QWidget):
         mainLayout.addWidget(self.nameLine2, 1, 1)
         mainLayout.addWidget(addressLabel, 2, 0, Qt.AlignTop)
         mainLayout.addWidget(self.addressText, 2, 1)
-        mainLayout.addLayout(buttonLayout1, 1, 2)
+        mainLayout.addLayout(buttonLayout1, 2, 2)
 
         self.setLayout(mainLayout)
         self.setWindowTitle("Information Retrieval")
@@ -47,6 +58,33 @@ class AddressBook(QWidget):
         self.nameLine.setFocus(Qt.OtherFocusReason)
         self.addressText.setReadOnly(False)
 
+    def loadFromFile(self):
+        fileName, _ = QFileDialog.getOpenFileName(self, "Open Address Book",
+                                                  '', "Address Book (*.abk);;All Files (*)")
+
+        if not fileName:
+            return
+
+        try:
+            in_file = open(str(fileName), 'rb')
+        except IOError:
+            QMessageBox.information(self, "Unable to open file",
+                                    "There was an error opening \"%s\"" % fileName)
+            return
+
+        self.contacts = pickle.load(in_file)
+        in_file.close()
+
+        if len(self.contacts) == 0:
+            QMessageBox.information(self, "No contacts in file",
+                                    "The file you are attempting to open contains no "
+                                    "contacts.")
+        else:
+            for name, address in self.contacts:
+                self.nameLine.setText(name)
+                self.addressText.setText(address)
+
+        self.updateInterface(self.NavigationMode)
 
 
 if __name__ == '__main__':
